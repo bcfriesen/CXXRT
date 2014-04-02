d9a2eef4883ccb1132cf919e84a11f0128fad4fd
Brian Friesen <friesen@ou.edu>
2014-04-02 12:46:11 -0500

    Set all rays to LTE.

--- src/main.cc
+++ src/main.cc
@@ -49,6 +49,7 @@ int main(int argc, char *argv[]) {
       mu += (mu_max - mu_min) / double(rays.size());
       r.calc_chi();
       r.calc_tau();
+      r.set_to_LTE();
       r.formal_soln();
     }
 
