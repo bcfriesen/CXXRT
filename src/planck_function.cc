1c7448b9a2d85f22e4dc552bee81f22006986ae9
Brian Friesen <friesen@ou.edu>
2014-04-16 14:02:16 -0500

    Changed "exp" and "pow" to "std::exp" and "std::pow" in planck_function().

--- src/planck_function.cc
+++ src/planck_function.cc
@@ -3,5 +3,5 @@
 #include "constants.hh"
 
 double planck_function(const double lambda, const double temperature) {
-  return (2.0 * h_planck * pow(c_light,2) / pow(lambda,5)) / (exp((h_planck * c_light) / (lambda * k_boltzmann * temperature)) - 1.0);
+  return (2.0 * h_planck * std::pow(c_light,2) / std::pow(lambda,5)) / (std::exp((h_planck * c_light) / (lambda * k_boltzmann * temperature)) - 1.0);
 }
