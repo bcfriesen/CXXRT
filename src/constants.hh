58fec49aeee6c9557827cd41162031e8d827ee81
Brian Friesen <friesen@ou.edu>
2014-06-13 10:43:05 -0500

    constants: added pi and the electron rest mass

--- src/constants.hh
+++ src/constants.hh
@@ -4,5 +4,7 @@
 const double h_planck = 6.6260755e-27;
 const double c_light = 2.99792458e+10;
 const double k_boltzmann = 1.380658e-16;
+const double pi = 3.1415927;
+const double m_electron = 9.10938291e-28;
 
 #endif
