bc8b83d0c546109df48523f9e8c9349b4a628f96
Brian Friesen <friesen@ou.edu>
2014-06-16 09:46:37 -0500

    lines: added missing semicolon

--- src/lines/Lorentzian_profile.cc
+++ src/lines/Lorentzian_profile.cc
@@ -3,5 +3,5 @@
 #include "../constants.hh"
 
 double lorentz_profile(const double lambda, const double lambda_0, const double Delta_lambda) {
-    return (1.0 / pi) * (0.5 * Delta_lambda) / (std::pow(lambda - lambda_0, 2) + std::pow(0.5 * Delta_lambda, 2))
+    return (1.0 / pi) * (0.5 * Delta_lambda) / (std::pow(lambda - lambda_0, 2) + std::pow(0.5 * Delta_lambda, 2));
 }
