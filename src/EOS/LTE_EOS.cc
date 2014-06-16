48b0a0f84e7f247edbdd8be3371724b138c411ac
Brian Friesen <friesen@ou.edu>
2014-06-16 12:28:10 -0500

    EOS: changed numerator of f_ij to use iterators instead of loop indices

--- src/EOS/LTE_EOS.cc
+++ src/EOS/LTE_EOS.cc
@@ -1,9 +1,19 @@
+#include <iostream>
+
 #include "saha_equation.hh"
 
 double f_ij (Atom atom, const double n_e, const double temperature) {
     double numerator = 1.0;
-    for (unsigned int i = 0; i < atom.ions.size()-1; ++i) {
-        numerator *= saha_equation(atom, i, n_e, temperature);
+    for (auto ion_it: atom.ions) {
+        // If the requested j+1'th ionization stage is more than fully ionized,
+        // then skip it. This case should happen only once: when trying to
+        // calculate the Saha equation where the lower ionization stage j is
+        // the fully-ionized atom.
+        if (ion_it.ionization_stage+1 > atom.atomic_number) {
+            continue;
+        } else {
+            numerator *= saha_equation(atom, ion_it.ionization_stage, n_e, temperature);
+        }
     }
 
     double denominator = 0.0;
