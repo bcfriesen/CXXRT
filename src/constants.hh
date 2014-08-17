084b2f9771415c2066d3f31a2ff5be18636091ce
Brian Friesen <friesen@ou.edu>
2014-08-17 17:53:44 -0500

    constants: added molar masses for all elements up to Zn

--- src/constants.hh
+++ src/constants.hh
@@ -11,6 +11,36 @@ const double ev2erg = 1.602177e-12;
 const double sigma_T = 6.6524e-25; // Thomson cross section
 const double N_A = 6.022e+23; // Avogadro's number
 
-const double H_molar_mass = 1.00794; // grams per mole
+// molar masses (grams per mole) taken from NIST's periodic table
+const double H_molar_mass  = 1.008;
+const double He_molar_mass = 4.002602;
+const double Li_molar_mass = 6.94;
+const double Be_molar_mass = 9.012182;
+const double B_molar_mass  = 10.81;
+const double C_molar_mass  = 12.011;
+const double N_molar_mass  = 14.007;
+const double O_molar_mass  = 15.999;
+const double F_molar_mass  = 18.9984032;
+const double Ne_molar_mass = 20.1797;
+const double Na_molar_mass = 22.98976928;
+const double Mg_molar_mass = 24.3050;
+const double Al_molar_mass = 26.9815386;
+const double Si_molar_mass = 28.085;
+const double P_molar_mass  = 30.973762;
+const double S_molar_mass  = 32.06;
+const double Cl_molar_mass = 35.45;
+const double Ar_molar_mass = 39.948;
+const double K_molar_mass  = 39.0983;
+const double Ca_molar_mass = 40.078;
+const double Sc_molar_mass = 44.955912;
+const double Ti_molar_mass = 47.867;
+const double V_molar_mass  = 50.9415;
+const double Cr_molar_mass = 51.9961;
+const double Mn_molar_mass = 54.938045;
+const double Fe_molar_mass = 55.845;
+const double Co_molar_mass = 58.933195;
+const double Ni_molar_mass = 58.6934;
+const double Cu_molar_mass = 63.546;
+const double Zn_molar_mass = 65.38;
 
 #endif
