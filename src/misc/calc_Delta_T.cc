e7c8614b62b2bd6f934857408ec17ea5b51817e8
Brian Friesen <friesen@ou.edu>
2014-08-18 15:32:57 -0500

    calc_Delta_t(): fixed mistake in calculating temperature correction

--- src/misc/calc_Delta_T.cc
+++ src/misc/calc_Delta_T.cc
@@ -12,10 +12,10 @@ double calc_Delta_T(GridVoxel gv) {
     for (auto gv_it = grid.rbegin(); gv_it != grid.rend()-1; ++gv_it) {
         // There's probably a more elegant way to say "do the integral until we get to the requested voxel".
         if (gv_it->z <= gv.z * (1.0 + std::numeric_limits<double>::epsilon())) {
-            const double integrand = -gv_it->calc_chi_H() * calc_H_target(*gv_it);
+            const double integrand = -gv_it->calc_chi_H() * (calc_H_target(*gv_it) - gv_it->calc_H_wl_integral());
             auto gv_it_next = gv_it;
             std::advance(gv_it_next, +1);
-            const double integrand_next = -gv_it_next->calc_chi_H() * calc_H_target(*gv_it_next);
+            const double integrand_next = -gv_it_next->calc_chi_H() * (calc_H_target(*gv_it_next) - gv_it_next->calc_H_wl_integral());
             result += 0.5 * (gv_it_next->z - gv_it->z) * (integrand_next - integrand);
         }
     }
