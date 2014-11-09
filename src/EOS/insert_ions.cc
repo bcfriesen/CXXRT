375907deffdb0fbde222085ecd05f35d2496cb24
Brian Friesen <friesen@ou.edu>
2014-11-08 20:59:50 -0600

    insert_ions(): added temporary constant variables to improve code readability

--- src/EOS/insert_ions.cc
+++ src/EOS/insert_ions.cc
@@ -6,12 +6,14 @@
 void insert_ions(std::vector<Atom> &atoms) {
     std::vector<unsigned int> ions;
     for (YAML::const_iterator it = config["atoms_with_max_ion"].begin(); it != config["atoms_with_max_ion"].end(); ++it) {
+        const std::string atomic_symbol = it->first.as<std::string>();
+        const unsigned int max_requested_ionization_stage = it->second.as<unsigned int>();
         // Make sure we don't try to add an atom which is more than fully ionized.
-        if (it->second.as<unsigned int>() >= atomic_symbols[it->first.as<std::string>()]) {
+        if (max_requested_ionization_stage >= atomic_symbols[atomic_symbol]) {
             std::cerr << "ERROR: you requested atom " << it->first.as<std::string>() << " with ionization stage " << it->second.as<unsigned int>() << std::endl;
             exit(1);
         }
-        Atom one_atom(atomic_symbols[it->first.as<std::string>()], it->second.as<unsigned int>());
+        Atom one_atom(atomic_symbols[atomic_symbol], max_requested_ionization_stage);
         atoms.push_back(one_atom);
     }
 
